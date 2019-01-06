da135e7e6f62eb33282df80dc2dad71b6a5880ec
oswaldobapvicjr <45291656+oswaldobapvicjr@users.noreply.github.com>
2019-01-06 19:37:30 -0200

    Improving Javadocs

--- src/main/java/net/obvj/performetrics/util/PerfrometricsUtils.java
+++ src/main/java/net/obvj/performetrics/util/PerfrometricsUtils.java
@@ -20,17 +20,20 @@ public class PerfrometricsUtils {
 	}
 
 	/**
-	 * Returns the current value of the current Java Virtual Machine's high-resolution time source in nanoseconds. 
-	 * time in nanoseconds.
+	 * Returns the current value of the current Java Virtual Machine's
+	 * high-resolution time source in nanoseconds. time in nanoseconds.
 	 * 
-	 * @return the difference, measured in nanoseconds between current time and some arbitrary origin time for the current JVM, that can be used for measuring elapsed times. 
+	 * @return the difference, measured in nanoseconds between current time and some
+	 *         arbitrary origin time for the current JVM, that can be used for
+	 *         measuring elapsed times.
 	 */
 	public static long getWallClockTimeNanos() {
 		return System.nanoTime();
 	}
 
 	/**
-	 * Returns the total CPU time for the current thread in nanoseconds.
+	 * Returns the total time spent using a CPU for the current thread in
+	 * nanoseconds.
 	 * 
 	 * @return the total CPU time for the current thread if CPU time measurement is
 	 *         enabled; 0 otherwise.
@@ -42,7 +45,7 @@ public class PerfrometricsUtils {
 
 	/**
 	 * Returns the CPU time that the current thread has executed in user mode in
-	 * nanoseconds.
+	 * nanoseconds (i.e., is the time spent running current thread's own code).
 	 * 
 	 * @return the user-level CPU time for the current thread if CPU time
 	 *         measurement is enabled; 0 otherwise.
@@ -53,8 +56,9 @@ public class PerfrometricsUtils {
 	}
 
 	/**
-	 * Returns the difference between CPU time and user time for the current thread
-	 * in nanoseconds.
+	 * Returns system time (the time spent running OS code on behalf of your
+	 * application, such as for I/O) in nanoseconds, by calculating the difference
+	 * between CPU time and user time for the current thread.
 	 * 
 	 * @return the system time for the current thread if CPU time measurement is
 	 *         enabled; 0 otherwise.

73144d45f9863e5c48abc1366f3bf82f5b66f27a
Oswaldo Baptista Vicente Junior <45291656+oswaldobapvicjr@users.noreply.github.com>
2023-03-04 01:12:50 -0300

    Update PrintUtilsTest.java

--- src/test/java/net/obvj/performetrics/util/print/PrintUtilsTest.java
+++ src/test/java/net/obvj/performetrics/util/print/PrintUtilsTest.java
@@ -32,6 +32,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +43,7 @@ import org.junit.jupiter.api.Test;
 
 import net.obvj.performetrics.Counter;
 import net.obvj.performetrics.Counter.Type;
+import net.obvj.performetrics.Performetrics;
 import net.obvj.performetrics.Stopwatch;
 import net.obvj.performetrics.config.ConfigurationHolder;
 import net.obvj.performetrics.util.DurationFormat;
@@ -56,11 +58,15 @@ class PrintUtilsTest
     private static final Counter C1 = newCounter(WALL_CLOCK_TIME, MILLISECONDS, 5000, 6000);
     private static final Counter C2 = newCounter(CPU_TIME, NANOSECONDS, 700000000000l, 900000000000l);
     private static final Map<Type, List<Counter>> ALL_COUNTERS = new EnumMap<>(Type.class);
+    private static final List<Type> ALL_TYPES = Arrays.asList(WALL_CLOCK_TIME, CPU_TIME);
+
+    private static final PrintStyle DEFAULT_PRINT_STYLE = PrintStyle.SUMMARIZED_XML;
 
     static
     {
         ALL_COUNTERS.put(WALL_CLOCK_TIME, singletonList(C1));
         ALL_COUNTERS.put(CPU_TIME, singletonList(C2));
+        Performetrics.setDefaultPrintStyle(DEFAULT_PRINT_STYLE);
     }
 
     Stopwatch stopwatch = mock(Stopwatch.class);
@@ -70,6 +76,9 @@ class PrintUtilsTest
     void setup()
     {
         when(stopwatch.getAllCountersByType()).thenReturn(ALL_COUNTERS);
+        when(stopwatch.getTypes()).thenReturn(ALL_TYPES);
+        when(stopwatch.elapsedTime(WALL_CLOCK_TIME)).thenReturn(C1.elapsedTime());
+        when(stopwatch.elapsedTime(CPU_TIME)).thenReturn(C2.elapsedTime());
     }
 
     private static Counter newCounter(Type type, TimeUnit timeUnit, long unitsBefore, long unitsAfter)
@@ -87,6 +96,23 @@ class PrintUtilsTest
     void constructor_instantiationNotAllowed()
     {
         assertThat(PrintUtils.class, instantiationNotAllowed().throwing(IllegalStateException.class));
+    }
+
+    /**
+     * Test stopwatch printing onto a PrintStream.
+     */
+    @Test
+    void print_withStopwatchAndPrintStream_printsTableToTheStream()
+            throws UnsupportedEncodingException
+    {
+        String expectedString = PrintFormat.SUMMARIZED.format(stopwatch, DEFAULT_PRINT_STYLE);
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos, true, "UTF-8");
+        PrintUtils.print(stopwatch, ps);
+        String printedString = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+
+        assertThat(printedString, is(equalTo(expectedString)));
     }
 
     /**
@@ -126,6 +152,20 @@ class PrintUtilsTest
         when(printStyle.getPrintFormat()).thenReturn(printFormat);
         when(printStyle.getDurationFormat()).thenReturn(DurationFormat.FULL);
         when(printStyle.getRowFormat()).thenReturn("%s");
+    }
+
+    @Test
+    void print_withStopwatchAndPrintStreamAndPrintStyle_printsToTheStream()
+            throws UnsupportedEncodingException
+    {
+        String expectedString = PrintFormat.SUMMARIZED.format(stopwatch, PrintStyle.SUMMARIZED_CSV);
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(baos, true, "UTF-8");
+        PrintUtils.print(stopwatch, printStream, PrintStyle.SUMMARIZED_CSV);
+        String printedString = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+
+        assertThat(printedString, is(equalTo(expectedString)));
     }
 
     @Test
@@ -190,12 +230,29 @@ class PrintUtilsTest
     {
         String expectedString = PrintFormat.SUMMARIZED.format(stopwatch,
                 ConfigurationHolder.getConfiguration().getPrintStyleForSummary());
-        String actualString = PrintUtils.toString(stopwatch);
+        String actualString = PrintUtils.summaryToString(stopwatch);
         assertThat(actualString, is(equalTo(expectedString)));
     }
 
     @Test
     void summaryToString_withStopwatchAndPrintStyle_validString()
+    {
+        String expectedString = PrintFormat.SUMMARIZED.format(stopwatch, PrintStyle.SUMMARIZED_CSV);
+        String actualString = PrintUtils.summaryToString(stopwatch, PrintStyle.SUMMARIZED_CSV);
+        assertThat(actualString, is(equalTo(expectedString)));
+    }
+
+    @Test
+    void toString_withStopwatch_validString()
+    {
+        String expectedString = PrintFormat.SUMMARIZED.format(stopwatch,
+                ConfigurationHolder.getConfiguration().getPrintStyle());
+        String actualString = PrintUtils.toString(stopwatch);
+        assertThat(actualString, is(equalTo(expectedString)));
+    }
+
+    @Test
+    void toString_withStopwatchAndPrintStyle_validString()
     {
         String expectedString = PrintFormat.SUMMARIZED.format(stopwatch, PrintStyle.SUMMARIZED_CSV);
         String actualString = PrintUtils.toString(stopwatch, PrintStyle.SUMMARIZED_CSV);

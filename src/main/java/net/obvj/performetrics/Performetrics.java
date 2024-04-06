66f10a5d3d26e500142302df6e53fc7f6a1b0ff5
Leonardo Soares <leonardo.arauujo46@gmail.com>
2024-04-06 01:16:58 -0300

    improving the new piece of the javadoc

--- src/main/java/net/obvj/performetrics/Performetrics.java
+++ src/main/java/net/obvj/performetrics/Performetrics.java
@@ -180,8 +180,8 @@ public class Performetrics
      * <p>
      * <b>Note:</b> If the provided {@link Runnable} throws an exception during execution,
      * the method will not be able to generate the monitored {@link MonitoredRunnable}.
-     * In this case, it's recommended to use the monitored {@link MonitoredRunnable} directly
-     * the same way the method is doing.
+     * In this case, it's recommended to use the monitored {@link MonitoredRunnable} directly.
+     * Then, the exception can be handled as necessary.
      *
      * @param runnable the {@link Runnable} to be run and monitored
      * @param types    the counter types to be measured in the operation
